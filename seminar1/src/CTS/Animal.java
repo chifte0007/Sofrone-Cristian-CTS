9c496ab7f832067c18e579b4ea2f91f0af43b769
ASUS <ASUS@ASUS-ROG>
2022-02-23 18:56:11 +0200

    Clasele seminar1

--- seminar1/src/CTS/Animal.java
+++ seminar1/src/CTS/Animal.java
@@ -3,5 +3,18 @@ package CTS;
 public abstract class Animal {
 	private String nume;
 	
+	public Animal(String nume) {
+		super();
+		this.nume = nume;
+	}
+
+	public String getNume() {
+		return nume;
+	}
+
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+
 	public abstract void mananca(String hrana); 
 }
